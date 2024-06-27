7d625c85411735285b31441e0fff3e95cb7e3a03
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-06-27 22:43:15 +0200

    Created and registered a controller, dispatched a route

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -4,14 +4,13 @@ declare(strict_types=1);
 
 require __DIR__ . "/../../vendor/autoload.php";
 
+use App\Controllers\HomeController;
 use Framework\App;
 
 $app = new App();
 
+$app->get('/phpiggy/public/', [HomeController::class, 'home']);
+
 $app->run();
-
-$app->get("/");
-
-dd($app);
 
 return $app;
