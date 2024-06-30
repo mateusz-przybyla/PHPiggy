a9cf60aab5655e42d24436ab3b7f078d2dc32d6a
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-06-30 11:25:15 +0200

    Created external definitions file

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 
 require __DIR__ . "/../../vendor/autoload.php";
 
+use App\Config\Paths;
 use App\Controllers\{HomeController, AboutController};
 use Framework\App;
 
-$app = new App();
+$app = new App(Paths::SOURCE . "App/container-definitions.php");
 
 $app->get('/', [HomeController::class, 'home']);
 $app->get('/about', [AboutController::class, 'about']);
