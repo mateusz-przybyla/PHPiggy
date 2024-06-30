9899c028b49d2f9697869e6c5e0f13ce66b2d8b0
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-06-30 09:25:03 +0200

    Implemented about page

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 require __DIR__ . "/../../vendor/autoload.php";
 
-use App\Controllers\HomeController;
+use App\Controllers\{HomeController, AboutController};
 use Framework\App;
 
 $app = new App();
 
 $app->get('/', [HomeController::class, 'home']);
+$app->get('/about', [AboutController::class, 'about']);
 
 $app->run();
 
