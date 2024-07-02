e2cc8dd5be2135fb152ed5a0ac6a2545af09b207
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-07-02 07:26:45 +0200

    Creating middleware and interface contracts

--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -7,6 +7,7 @@ namespace Framework;
 class Router
 {
   private array $routes = [];
+  private array $middlewares = [];
 
   public function add(string $method, string $path, array $controller)
   {
@@ -42,5 +43,10 @@ class Router
       $controllerInstance = $container ? $container->resolve($class) : new $class;
       $controllerInstance->$function();
     }
+  }
+
+  public function addMiddleware(string $middleware)
+  {
+    $this->middlewares[] = $middleware;
   }
 }
