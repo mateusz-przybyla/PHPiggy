793cc88ca293d1541370d8f44505672ed8b6a26b
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-07-04 22:11:38 +0200

    Implemented register page

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 require __DIR__ . "/../../vendor/autoload.php";
 
 use App\Config\Paths;
-use App\Controllers\{HomeController, AboutController};
+use App\Controllers\{HomeController, AboutController, RegisterController};
 use Framework\App;
 use function App\Config\{registerMiddleware};
 
@@ -13,6 +13,7 @@ $app = new App(Paths::SOURCE . "App/container-definitions.php");
 
 $app->get('/', [HomeController::class, 'home']);
 $app->get('/about', [AboutController::class, 'about']);
+$app->get('/register', [RegisterController::class, 'register']);
 
 registerMiddleware($app);
 
