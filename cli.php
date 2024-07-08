10f57a642374153c7995408d78eaaf2155bd795a
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-07-08 22:28:16 +0200

    Creating a DSN string

--- cli.php
+++ cli.php
@@ -1,3 +1,12 @@
 <?php
 
-echo "Hello world!";
+$driver = 'mysql';
+$config = http_build_query(data: [
+  'host' => 'localhost',
+  'port' => 3306,
+  'dbname' => 'phpiggy'
+], arg_separator: ';');
+
+$dsn = "{$driver}:{$config}";
+
+echo $dsn;
