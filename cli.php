3b5e9d4513eae07099d7613c3793be5993f0c59f
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-07-09 21:54:05 +0200

    Creating transaction with PDO

--- cli.php
+++ cli.php
@@ -28,8 +28,25 @@ $db = new Database('mysql', [
   'dbname' => 'phpiggy'
 ], 'root', '');
 
-$query = "SELECT * FROM products";
+try {
+  $db->connection->beginTransaction();
 
-$stmt = $db->connection->query($query);
+  $db->connection->query("INSERT INTO products VALUES(99, 'Gloves')");
 
-var_dump($stmt->fetchAll());
+  $search = "Hats";
+  $query = "SELECT * FROM products WHERE name = :name";
+
+  $stmt = $db->connection->prepare($query);
+  $stmt->bindValue('name', 'Gloves', PDO::PARAM_STR);
+  $stmt->execute();
+
+  var_dump($stmt->fetchAll(PDO::FETCH_ASSOC));
+
+  $db->connection->commit();
+} catch (Exception $error) {
+  if ($db->connection->inTransaction()) {
+    $db->connection->rollBack();
+  }
+
+  echo "Transaction failed!.";
+}
