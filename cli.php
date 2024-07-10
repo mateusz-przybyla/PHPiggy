9717aa0b0f233e8af2bf282d404d16b61b48ff67
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-07-10 21:57:36 +0200

    Creating environment variabes

--- cli.php
+++ cli.php
@@ -18,15 +18,20 @@ try {
 }
 */
 
-include __DIR__ . '/src/Framework/Database.php';
+require __DIR__ . '/vendor/autoload.php';
 
 use Framework\Database;
+use App\Config\Paths;
+use Dotenv\Dotenv;
 
-$db = new Database('mysql', [
-  'host' => 'localhost',
-  'port' => 3306,
-  'dbname' => 'phpiggy'
-], 'root', '');
+$dotenv = Dotenv::createImmutable(Paths::ROOT);
+$dotenv->load();
+
+$db = new Database($_ENV['DB_DRIVER'], [
+  'host' => $_ENV['DB_HOST'],
+  'port' => $_ENV['DB_PORT'],
+  'dbname' => $_ENV['DB_NAME']
+], $_ENV['DB_USER'], $_ENV['DB_PASS']);
 
 $sqlFile = file_get_contents("./database.sql");
 
