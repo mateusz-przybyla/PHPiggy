61b4ff6495d31e6f35f09e1d76f09937774caab9
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-07-13 10:50:21 +0200

    Preparing and validating login page

---
+++ src/App/Controllers/AuthController.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use Framework\TemplateEngine;
+use App\Services\{
+  ValidatorService,
+  UserService
+};
+
+class AuthController
+{
+  public function __construct(
+    private TemplateEngine $view,
+    private ValidatorService $validatorService,
+    private UserService $userService
+  ) {
+  }
+
+  public function registerView()
+  {
+    echo $this->view->render("register.php");
+  }
+
+  public function register()
+  {
+    $this->validatorService->validateRegister($_POST);
+
+    $this->userService->isEmailTaken($_POST['email']);
+
+    $this->userService->createNewUser($_POST);
+
+    redirectTo('/');
+  }
+
+  public function loginView()
+  {
+    echo $this->view->render("login.php");
+  }
+
+  public function login()
+  {
+    $this->validatorService->validateLogin($_POST);
+
+    $this->userService->verifyLoginData($_POST);
+
+    redirectTo('/');
+  }
+}
