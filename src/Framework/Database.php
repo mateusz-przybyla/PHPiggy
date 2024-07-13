698adb850a769a7989833426ddaa0ab74bb359f4
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-07-13 11:09:34 +0200

    Deleted RegisterController

--- src/Framework/Database.php
+++ src/Framework/Database.php
@@ -22,7 +22,9 @@ class Database
     $dsn = "{$driver}:{$config}";
 
     try {
-      $this->connection = new PDO($dsn, $username, $password);
+      $this->connection = new PDO($dsn, $username, $password, [
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+      ]);
     } catch (PDOException $e) {
       die("Unable to connect to database");
     }
@@ -40,5 +42,10 @@ class Database
   public function count()
   {
     return $this->stmt->fetchColumn();
+  }
+
+  public function retrieve()
+  {
+    return $this->stmt->fetch();
   }
 }
