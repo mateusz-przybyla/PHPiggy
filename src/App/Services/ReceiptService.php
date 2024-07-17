c5917579ad705cf1c7ecdf975e318d82eb4a1666
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-07-17 12:35:07 +0200

    Validating uploaded files

--- src/App/Services/ReceiptService.php
+++ src/App/Services/ReceiptService.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use Exception;
 use Framework\Database;
+use Framework\Exceptions\ValidationException;
 
 class ReceiptService
 {
@@ -14,6 +16,35 @@ class ReceiptService
 
   public function validateFile(?array $file)
   {
-    dd($file);
+    if (!$file || $file['error'] !== UPLOAD_ERR_OK) {
+      throw new ValidationException([
+        'receipt' => ['Failed to upload file.']
+      ]);
+    }
+
+    $maxFileSizeMB = 3 * 1024 * 1024;
+
+    if ($file['size'] > $maxFileSizeMB) {
+      throw new ValidationException([
+        'receipt' => ["File upload is too large."]
+      ]);
+    }
+
+    $originalFileName = $file['name'];
+
+    if (!preg_match('/^[A-Za-z0-9\s._-]+$/', $originalFileName)) {
+      throw new ValidationException([
+        'receipt' => ["Invalid filename."]
+      ]);
+    }
+
+    $clientMimeType = $file['type'];
+    $allowedMimeTypes = ['image/jpeg', 'image/png', 'application/pdf'];
+
+    if (!in_array($clientMimeType, $allowedMimeTypes)) {
+      throw new ValidationException([
+        'receipt' => ['Invalid file type.']
+      ]);
+    }
   }
 }
